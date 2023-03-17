cb288b5cd5dfc26e73361f76be446578f7bd0bd2
Sky <skylarplayz348@gmail.com>
2023-03-17 11:16:47 -0500

    whoops meant to change this

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
     # Database CONFIG
     $dbHost = "localhost"; # Use this unless your SQL server is on another server
-    $dbUser = "USER"; # Don't recommend this but is needed for local host with XAMPP
+    $dbUser = "root"; # Don't recommend this but is needed for local host with XAMPP
     $dbPwd = ""; # Again don't recommend this but is needed for local host with XAMPP
     $dbName = "php_example_login"; # Name of database in database.sql
     
